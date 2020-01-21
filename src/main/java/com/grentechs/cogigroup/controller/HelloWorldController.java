df6914be7583bf51c80f58e40d11a2508a928906
ngomouz <ousmane.ngom09@gmail.com>
2020-01-21 12:56:27 +0100

    hello world method which return a bean

--- src/main/java/com/grentechs/cogigroup/controller/HelloWorldController.java
+++ src/main/java/com/grentechs/cogigroup/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.grentechs.cogigroup.controller;
 
+import com.grentechs.cogigroup.entities.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "hello World";
+    }
+
+    @GetMapping(path = "/helloworld-bean")
+    public UserDetails getUserDetails() {
+        return new UserDetails("Ousmane", "NGOM", "PARIS");
     }
 }
---
+++ src/main/java/com/grentechs/cogigroup/entities/UserDetails.java
@@ -0,0 +1,50 @@
+package com.grentechs.cogigroup.entities;
+
+public class UserDetails {
+
+    private String firstname;
+    private String lastname;
+    private String city;
+
+    public UserDetails() {
+    }
+
+    public UserDetails(String firstname, String lastname, String city) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.city = city;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDetails{" +
+            "firstname='" + firstname + '\'' +
+            ", lastname='" + lastname + '\'' +
+            ", city='" + city + '\'' +
+        '}';
+    }
+}
