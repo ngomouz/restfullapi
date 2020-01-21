ddf3c270382568884e8122ae8d8741e097553630
ngomouz <ousmane.ngom09@gmail.com>
2020-01-21 15:55:05 +0100

    User management services

--- src/main/java/com/grentechs/cogigroup/controller/UserController.java
+++ src/main/java/com/grentechs/cogigroup/controller/UserController.java
@@ -2,11 +2,11 @@ package com.grentechs.cogigroup.controller;
 
 import com.grentechs.cogigroup.entities.User;
 import com.grentechs.cogigroup.services.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/users")
@@ -21,5 +21,30 @@ public class UserController {
     @GetMapping(path = "/getAllUser")
     public List<User> getAllUser() {
         return userService.getAllUser();
+    }
+
+    @PostMapping(path = "/createUser")
+    public User createUser(@RequestBody @Valid User user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping(path = "/getUserById/{id}")
+    public Optional<User> getUserById(@PathVariable(value = "id") Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PutMapping(path = "/updateUserById/{id}")
+    public User updateUserById(@PathVariable(value = "id", required = true) Long id, @RequestBody @Valid User user) {
+        return userService.updateUserById(id, user);
+    }
+
+    @DeleteMapping(path = "/deleteUserById/{id}")
+    public void deleteUserById(@PathVariable(value = "id", required = true) Long id) {
+        userService.deleteUserById(id);
+    }
+
+    @GetMapping(path = "/getUserByUsername/{username}")
+    public User getUserByUsername(@PathVariable(value = "username", required = true) String username) {
+        return userService.getUserByUsername(username);
     }
 }
--- src/main/java/com/grentechs/cogigroup/services/UserService.java
+++ src/main/java/com/grentechs/cogigroup/services/UserService.java
@@ -5,6 +5,7 @@ import com.grentechs.cogigroup.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -17,5 +18,27 @@ public class UserService {
 
     public List<User> getAllUser() {
         return userRepository.findAll();
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User updateUserById(Long id, User user) {
+        user.setId(id);
+        return userRepository.save(user);
+    }
+
+    public void deleteUserById(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        user.ifPresent(value -> userRepository.delete(value));
+    }
+
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
     }
 }
