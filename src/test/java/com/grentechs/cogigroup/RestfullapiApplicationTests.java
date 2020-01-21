d26f0595f1d20e94432f75d9009946580cd92963
ngomouz <ousmane.ngom09@gmail.com>
2020-01-21 12:13:33 +0100

    initial commit

---
+++ src/test/java/com/grentechs/cogigroup/RestfullapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grentechs.cogigroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
